b407af0145ce8ca1d4a7fe558bca55a06a15f050
Prospero <60527636+Prosperos-Masq@users.noreply.github.com>
2020-12-17 03:30:46 -0600

    power update
    added power option and cleaned up code

--- calculator.py
+++ calculator.py
@@ -1,26 +1,32 @@
-while True:
+calculator = True
+while calculator:
     start = input("Hello! I'm your calculator would you like to use me (y/n)?: ").strip().lower()
     if start == "y" or "yes":
-        calculate = input("Would you like to Multiply/Divide/Add/Subtract? :").strip().lower()
+        calculate = input("Would you like to Multiply/Divide/Add/Subtract/Power? :").strip().lower()
         if calculate == "add":
             num1 = input("To add please enter a number: ").strip()
             num2 = input("Please enter another number: ").strip()
-            answer = int(num1) + int(num2)
-            print("The answer is {}!".format(answer))
+            add = int(num1) + int(num2)
+            print("{} + {} is {}!".format(num1, num2, add))
         elif calculate == "subtract":
             num1 = input("To subtract a number please enter a number: ").strip()
             num2 = input("Please enter another number: ").strip()
-            answer = int(num1) - int(num2)
-            print("The answer is {}!".format(answer))
+            subtract = int(num1) - int(num2)
+            print("{} - {} is {}!".format(num1, num2, subtract))
         elif calculate == "multiply":
             num1 = input("To multiply a number please enter a number: ").strip()
             num2 = input("Please enter another number: ").strip()
             multiply = int(num1) * int(num2)
-            print("The answer is {}!".format(multiply))
+            print("{} x {} is {}!".format(num1, num2, multiply))
         elif calculate == "divide":
             num1 = input("To divide a number please enter a number: ").strip()
             num2 = input("Please enter another number: ").strip()
-            answer = int(num1) / int(num2)
-            print("The answer is {}!".format(answer))
+            divide = int(num1) / int(num2)
+            print("{} / {} is {}!".format(num1, num2, divide))
+        elif calculate == "power":
+            num1 = input("To add please enter a number: ").strip()
+            num2 = input("Please enter another number: ").strip()
+            power = int(num1) ** int(num2)
+            print("{} to the power of {} is: {}!".format(num1, num2, power))
     else:
          print("Okay! Another time then!")
